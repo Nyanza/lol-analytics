9fcb56eec80ab992087bcd7bdf90ad7b4c785b0a
misspia <pia_leung@hotmail.com>
2017-09-16 22:23:01 -0400

    configure webpack to compile react

---
+++ lol-api/src/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+import './index.scss';
+
+class App extends Component {
+	render() {
+		return <div className="app">
+			yayyy i high tech resume soon come
+		</div>;
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
+
+
+
+
---
+++ lol-api/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const ROOT_PATH = path.resolve(__dirname);
+const OUTPUT_PATH = path.resolve(ROOT_PATH, 'dist');
+const TEMPLATE_PATH = path.resolve(ROOT_PATH, 'index.html');
+const ENTRY_PATH = path.resolve(ROOT_PATH, 'src/index.jsx');
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: ENTRY_PATH,
+	output: {
+		filename: 'bundle.js',
+		path: OUTPUT_PATH,
+		publicPath: 'dist/'
+	},
+	plugins: [
+		new ExtractTextPlugin('bundle.css')
+	],
+	module: {
+		rules: [{
+			test: /\.(js|jsx)$/, 
+			exclude: /node_modules/,
+			use: [
+			'jshint-loader',
+			{
+				loader: 'babel-loader',
+				options: {
+					presets: [
+					['es2015', { modules: false }],
+					'react',
+					'stage-3'
+					]
+				}
+			}
+			]
+		},
+		{
+			test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: 'css-loader!sass-loader'
+			})
+		},
+		{
+			test: /\.(png|jpeg|gif|ttf)$/,
+			use: [
+				{ loader: 'url-loader', options: {} } 
+			]
+		}
+		]
+	}
+};
