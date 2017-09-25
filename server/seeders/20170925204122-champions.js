8c2e7664c7693a7baab714a3b1a1d939780d65a7
User <sibtain_95@hotmail.com>
2017-09-25 16:49:16 -0400

    added champions.json

---
+++ server/seeders/20170925204122-champions.js
@@ -0,0 +1,29 @@
+const request = require("request")
+
+//request.get('http://google.com/img.png')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+
+    return queryInterface.bulkInsert('Person', [{
+      name: 'John Doe',
+      isBetaMember: false
+    }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
