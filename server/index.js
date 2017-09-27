4d7867d6b1e11644b16ebd4c2cda8f00c2f3dd19
misspia <pia_leung@hotmail.com>
2017-09-27 19:54:39 -0400

    router router fallback using * in express SSR

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const http = require('http');
 const path = require('path')
 const app = express();
-
 const port = parseInt(process.env.PORT, 10) || 8000;
-const server = http.createServer(app);
 
 const ChampionsRouter = require('./routes/champions.js');
 const ImagesRouter = require('./routes/images.js');
@@ -32,11 +29,8 @@ app.use('/api/v1/spells', SpellsRouter);
 app.use('/api/v1/stats', StatsRouter);
 
 app.use('/dist', express.static(path.join(__dirname, '/../dist')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/../index.html')) );
+app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/../index.html')) );
 
-
-
-app.set('port', port);
-server.listen(port, () => {
-    console.log("Express server listening on port " + port);
+app.listen(port, () => {
+	console.log('Listening on port ', port);
 });
